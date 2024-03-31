6922fa74c121e062d399a05f1425596ca64ee3df
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-03-31 09:51:37 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,11 +3,11 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
+	API_ID = int(os.environ.get("API_ID", "26945844"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002020948245"))
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "5631563685"))
